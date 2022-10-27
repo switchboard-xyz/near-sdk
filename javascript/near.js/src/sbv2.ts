96771423340177fb5973a2c8556b939e486c37f6
Conner Gallagher <ConnerNGallagher@gmail.com>
2022-10-26 22:40:22 -0600

    re-export generated SwitchboardDecimal

--- javascript/near.js/src/sbv2.ts
+++ javascript/near.js/src/sbv2.ts
@@ -17,6 +17,9 @@ import * as actions from "./actions.js";
 import { roClient, SwitchboardProgram } from "./program.js";
 import { DEFAULT_FT_STORAGE_DEPOSIT } from "./token";
 import { fromBase58, isBase58, parseAddressString } from "./utils.js";
+import { SwitchboardDecimal } from "./generated/types/SwitchboardDecimal.js";
+
+export { SwitchboardDecimal } from "./generated/types/SwitchboardDecimal.js";
 
 Big.DP = 40;
 
@@ -124,10 +127,10 @@ export class AggregatorAccount {
           minJobResults: params.minJobResults,
           minUpdateDelaySeconds: params.minUpdateDelaySeconds,
           startAfter: new BN(params.startAfter),
-          varianceThreshold: new types.SwitchboardDecimal({
-            mantissa: params.varianceThreshold.mantissa,
-            scale: params.varianceThreshold.scale,
-          }),
+          varianceThreshold: new SwitchboardDecimal(
+            params.varianceThreshold.mantissa,
+            params.varianceThreshold.scale
+          ),
           forceReportPeriod: new BN(params.forceReportPeriod),
           crank: params.crank ?? new Uint8Array(32),
           expiration: new BN(0),
@@ -284,10 +287,10 @@ export class AggregatorAccount {
           startAfter:
             params === undefined ? undefined : new BN(params.startAfter),
           varianceThreshold: params.varianceThreshold
-            ? new types.SwitchboardDecimal({
-                mantissa: params.varianceThreshold.mantissa,
-                scale: params.varianceThreshold.scale,
-              })
+            ? new SwitchboardDecimal(
+                params.varianceThreshold.mantissa,
+                params.varianceThreshold.scale
+              )
             : undefined,
           forceReportPeriod:
             params.forceReportPeriod === undefined
@@ -802,10 +805,7 @@ export class QueueAccount {
           oracleTimeout: params.oracleTimeout,
           name: (params.name ?? Buffer.from("")).slice(0, 32),
           metadata: params.metadata ?? Buffer.from(""),
-          varianceToleranceMultiplier: new types.SwitchboardDecimal({
-            mantissa: new BN(0),
-            scale: 0,
-          }),
+          varianceToleranceMultiplier: new SwitchboardDecimal(new BN(0), 0),
           feedProbationPeriod: params.feedProbationPeriod ?? 0,
           slashingEnabled: params.slashingEnabled ?? false,
           consecutiveFeedFailureLimit: new BN(
@@ -880,10 +880,10 @@ export class QueueAccount {
           slashingEnabled: params.slashingEnabled,
           varianceToleranceMultiplier:
             params.varianceToleranceMultiplier !== undefined
-              ? new types.SwitchboardDecimal({
-                  mantissa: params.varianceToleranceMultiplier.mantissa,
-                  scale: params.varianceToleranceMultiplier.scale,
-                })
+              ? new SwitchboardDecimal(
+                  params.varianceToleranceMultiplier.mantissa,
+                  params.varianceToleranceMultiplier.scale
+                )
               : undefined,
           consecutiveFeedFailureLimit: params.consecutiveFeedFailureLimit,
           consecutiveOracleFailureLimit: params.consecutiveOracleFailureLimit,
@@ -1617,109 +1617,4 @@ export enum SwitchboardPermission {
   PERMIT_ORACLE_HEARTBEAT = 1 << 0,
   PERMIT_ORACLE_QUEUE_USAGE = 1 << 1,
   PERMIT_VRF_REQUESTS = 1 << 2,
-}
-
-/**
- * Switchboard precisioned representation of numbers.
- */
-export class SwitchboardDecimal {
-  public constructor(
-    public readonly mantissa: BN,
-    public readonly scale: number
-  ) {}
-
-  /**
-   * Convert untyped object to a Switchboard decimal, if possible.
-   * @param obj raw object to convert from
-   * @return SwitchboardDecimal
-   */
-  public static from(obj: any): SwitchboardDecimal {
-    return new SwitchboardDecimal(new BN(obj.mantissa), obj.scale);
-  }
-
-  /**
-   * Convert a Big.js decimal to a Switchboard decimal.
-   * @param big a Big.js decimal
-   * @return a SwitchboardDecimal
-   */
-  public static fromBig(big: Big): SwitchboardDecimal {
-    // Round to fit in Switchboard Decimal
-    // TODO: smarter logic.
-    big = big.round(20);
-    let mantissa: BN = new BN(big.c.join(""), 10);
-    // Set the scale. Big.exponenet sets scale from the opposite side
-    // SwitchboardDecimal does.
-    let scale = big.c.slice(1).length - big.e;
-
-    if (scale < 0) {
-      mantissa = mantissa.mul(new BN(10, 10).pow(new BN(Math.abs(scale), 10)));
-      scale = 0;
-    }
-    if (scale < 0) {
-      throw new Error(`SwitchboardDecimal: Unexpected negative scale.`);
-    }
-    if (scale >= 28) {
-      throw new Error("SwitchboardDecimalExcessiveScaleError");
-    }
-
-    // Set sign for the coefficient (mantissa)
-    mantissa = mantissa.mul(new BN(big.s, 10));
-
-    const result = new SwitchboardDecimal(mantissa, scale);
-    if (big.sub(result.toBig()).abs().gt(new Big(0.00005))) {
-      throw new Error(
-        `SwitchboardDecimal: Converted decimal does not match original:\n` +
-          `out: ${result.toBig().toNumber()} vs in: ${big.toNumber()}\n` +
-          `-- result mantissa and scale: ${result.mantissa.toString()} ${result.scale.toString()}\n` +
-          `${result} ${result.toBig()}`
-      );
-    }
-    return result;
-  }
-
-  /**
-   * SwitchboardDecimal equality comparator.
-   * @param other object to compare to.
-   * @return true iff equal
-   */
-  public eq(other: SwitchboardDecimal): boolean {
-    return this.mantissa.eq(other.mantissa) && this.scale === other.scale;
-  }
-
-  /**
-   * Convert SwitchboardDecimal to big.js Big type.
-   * @return Big representation
-   */
-  public toBig(): Big {
-    let mantissa: BN = new BN(this.mantissa, 10);
-    let s = 1;
-    const c: Array<number> = [];
-    const ZERO = new BN(0, 10);
-    const TEN = new BN(10, 10);
-    if (mantissa.lt(ZERO)) {
-      s = -1;
-      mantissa = mantissa.abs();
-    }
-    while (mantissa.gt(ZERO)) {
-      c.unshift(mantissa.mod(TEN).toNumber());
-      mantissa = mantissa.div(TEN);
-    }
-    const e = c.length - this.scale - 1;
-    const result = new Big(0);
-    if (c.length === 0) {
-      return result;
-    }
-    result.s = s;
-    result.c = c;
-    result.e = e;
-    return result;
-  }
-
-  toString() {
-    return this.toBig().toString();
-  }
-
-  toNearDecimal(): any {
-    return { mantissa: this.mantissa.toString(), scale: this.scale };
-  }
 }
